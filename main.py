f2633c694a5f4a82ca4be77d8cfdd3d64cbace18
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-30 12:33:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+m=int(input())
+n=int(input())
+counter=0
+sum_of_numbers=0
+while counter<n:
+    number=counter+m 
+    sum_of_numbers=sum_of_numbers+number
+    counter=counter+1
+print(sum_of_numbers)
+
